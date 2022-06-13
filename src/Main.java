59c8d4d9ddceb86d8af738893c1535e05a162729
Kostya <snaaack@.mail.ru>
2022-06-13 02:20:37 +0200

    download old homework and add to gitignore dir out

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
+
+        double ticketPrice; // Цена билета с копейками
+        int priceOfOneMile; // Стоимость одной бонусной мили
+        int bonusMiles; // Количество полученных бонусных миль
+
+        ticketPrice = 529.99;
+        priceOfOneMile = 20;
+
+        int ticketPriceInRub = (int) ticketPrice; // Цена билета в рублях без копеек
+
+        bonusMiles = ticketPriceInRub / priceOfOneMile;
+
+        System.out.println("За приобретенный Вами билет стоимостью: " + ticketPriceInRub + " рублей, Вы получите: " + bonusMiles + " бонусных миль");
+    }
 }

e4e03c8b5d7107a8c8a9f84daaf183372b50c545
Kostya <snaaack@.mail.ru>
2022-06-15 00:36:34 +0200

    Reformat code - method BonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate (int cost ) {
+    public int calculate(int cost) {
         int priceOfOneMile = 20;
-        int bonusMiles = cost/priceOfOneMile;
+        int bonusMiles = cost / priceOfOneMile;
         return bonusMiles;
     }
 }
